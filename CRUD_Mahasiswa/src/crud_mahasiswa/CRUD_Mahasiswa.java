85de9c55749e850fcd00c6c388ee45c8710edd3b
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2026-04-26 10:55:02 +0700

    Add files via upload

---
+++ CRUD_Mahasiswa/src/crud_mahasiswa/CRUD_Mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crud_mahasiswa;
+
+/**
+ *
+ * @author macbookair
+ */
+public class CRUD_Mahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new Data_Mahasiswa().setVisible(true);
+    }
+    
+}
